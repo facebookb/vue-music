eb41404141789564a200e1c514944b04dc4d6097
liyang <facebookb@163.com>
2018-01-29 11:40:14 +0800

    解决编译规则错误

--- src/store/state.js
+++ src/store/state.js
@@ -3,17 +3,17 @@ import {loadSearch, loadPlay, loadFavorite} from 'common/js/cache'
 
 const state = {
   singer: {}, /* 歌手 */
-  playing: false,/* 是否播放 */
-  fullScreen: false,/* 是否大屏 */
-  playlist: [],/* 播放列表 */
-  sequenceList: [],/*  */
-  mode: playMode.sequence,/* 播放模式，默认为顺序 */
-  currentIndex: -1,/* 当前index */
-  disc: {},/*  */
-  topList: {},/* 排行榜 */
-  searchHistory: loadSearch(),/* 搜索历史记录 */
-  playHistory: loadPlay(),/* 播放历史记录 */
-  favoriteList: loadFavorite()/* 收藏列表 */
+  playing: false, /* 是否播放 */
+  fullScreen: false, /* 是否大屏 */
+  playlist: [], /* 播放列表 */
+  sequenceList: [], /*  */
+  mode: playMode.sequence, /* 播放模式，默认为顺序 */
+  currentIndex: -1, /* 当前index */
+  disc: {}, /*  */
+  topList: {}, /* 排行榜 */
+  searchHistory: loadSearch(), /* 搜索历史记录 */
+  playHistory: loadPlay(), /* 播放历史记录 */
+  favoriteList: loadFavorite() /* 收藏列表 */
 }
 
 export default state
